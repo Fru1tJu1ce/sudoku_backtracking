9df49b585ccde68d7b3132303e725d2ffbb9a2fc
Fru1tJu1ce <83663507+Fru1tJu1ce@users.noreply.github.com>
2021-09-25 05:49:43 +0300

    Add files via upload

---
+++ button.py
@@ -0,0 +1,37 @@
+import pygame.font
+
+
+class Button:
+    """Класс для создания кнопки."""
+
+    def __init__(self, screen, msg):
+        """Инициализирует атрибуты кнопки."""
+        self.screen = screen
+
+        # Назначение размеров и свойств кнопок.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 0, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Построение объекта rect кнопки и размещение в правой нижней части экрана
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.x = 772
+        self.rect.y = 679
+
+        # Сообщение кнопки создается только один раз
+        self.prep_msg(msg)
+
+    def prep_msg(self, msg):
+        """
+        Преобразует msg в прямоугольник и выравнивает текст по центру.
+        """
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                                          self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        """Отображение пустой кнопки и вывод сообщения."""
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
