df243e437915556f3e55c7189d7eddf069cb31c1
Fru1tJu1ce <83663507+Fru1tJu1ce@users.noreply.github.com>
2021-09-25 05:51:07 +0300

    Update sudoku_functions.py

--- sudoku_functions.py
+++ sudoku_functions.py
@@ -4,13 +4,16 @@ import time
 import pygame
 
 
-def check_events():
+def check_events(button, su_settings):
     """Обрабатывает события нажатий клавиш."""
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             check_keydown_events(event)
-        if event.type == pygame.QUIT:
+        elif event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(button, su_settings, mouse_x, mouse_y)
 
 
 def check_keydown_events(event):
@@ -79,9 +82,9 @@ def check_rules(sudoku, value, i, j):
     return True
 
 
-def solve(screen, su_settings, field, timer):
+def solve(screen, su_settings, field, timer, button):
     """Ищет решение судоку."""
-    check_events()
+    check_events(button, su_settings)
     contents = zero_search(su_settings.sudoku)
     if not contents:
         return True
@@ -98,7 +101,7 @@ def solve(screen, su_settings, field, timer):
         if check_rules(su_settings.sudoku, value, i, j):
             su_settings.sudoku[i][j] = value
 
-            if solve(screen, su_settings, field, timer):
+            if solve(screen, su_settings, field, timer, button):
                 return True
         value += 1
 
@@ -142,3 +145,10 @@ def draw_timer(timer):
     new_time = int(time.time() - timer.time)
     timer.update_time(new_time)
     timer.draw_timer()
+
+
+def check_play_button(button, su_settings, mouse_x, mouse_y):
+    """Запускает новую игру при нажатии кнопки Play."""
+    button_clicked = button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked:
+        su_settings.solving_active = True
